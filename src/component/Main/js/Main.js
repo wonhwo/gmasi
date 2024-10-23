119886d272d8f72da0937a30d8e2ec22b64121c9
wonhwo <dlgy9714@gmail.com>
2024-10-23 17:55:34 +0900

    클릭이후 위로 이동 까지 이후 제목 서서히보이게 할 것

--- src/component/Main/js/Main.js
+++ src/component/Main/js/Main.js
@@ -1,5 +1,5 @@
 import '../scss/Main.scss'
-import {useState} from "react";
+import {useRef, useState} from "react";
 
 const Main = () => {
     const data = [
@@ -16,9 +16,10 @@ const Main = () => {
         {id: 'data11', year: 2026},
     ];
 
-
+    const [isMoveTimeLine, setIsMoveTimeLine] = useState(false);
     const [isIconHover, seIsIconHover] = useState("");
     const iconMouseHoverEventHandler = (e) => {
+        getPositonHandler(e);
         let temp = e.target.classList[1];
         seIsIconHover(temp);
     }
@@ -26,6 +27,7 @@ const Main = () => {
         seIsIconHover("")
     }
     const getPositonHandler = (e) => {
+
         const clickedElement = e.currentTarget;
         // 클릭한 요소의 위치 계산
         const elementRect = clickedElement.getBoundingClientRect();
@@ -47,32 +49,50 @@ const Main = () => {
         });
     }
 
+    const clickEventHandler = () => {
+        const dataElements = document.querySelectorAll('.timeLineBottom');
+
+        setIsMoveTimeLine(false);
+        dataElements.forEach((element) => {
+            element.classList.add('removeCircle');
+        });
+        setIsMoveTimeLine(true);
+
+
+
+    }
+    const downTimeLineHandler = () => {
+        const dataElements = document.querySelectorAll('.timeLineBottom');
+
+        setIsMoveTimeLine(false);
+        dataElements.forEach((element) => {
+            element.classList.remove('removeCircle');
+        });
+
+    }
+
     return (
         <>
             <section id="timeLineContainer">
-                <div className="timeLine timeLineTop"></div>
+                <div className={`timeLine timeLineTop ${isMoveTimeLine ? "timeLineBottomAnimate" : ""}`} onClick={downTimeLineHandler}>
+                </div>
                 <hr/>
                 <div className="timeLine timeLineBottom">
                     {data.map((item) => (
                         <div className={`data ${item.id}`}>
-                            {isIconHover !== item.id ? (
-                                <img
-                                    src='/img/circle.png'
-                                    className={`circleIcon ${item.id}`}
-                                    onMouseEnter={iconMouseHoverEventHandler}
-                                    onMouseLeave={iconMouseExitEventHandler}
-                                    onClick={getPositonHandler}
-                                    alt="이미지 깨짐"
-                                />
-                            ) : (
-                                <img
-                                    src="/img/down.png"
-                                    className={`circleIcon ${item.id}`}
-                                    onMouseEnter={iconMouseHoverEventHandler}
-                                    onMouseLeave={iconMouseExitEventHandler} onClick={getPositonHandler}
-                                    alt="이미지 깨짐"
-                                />
-                            )}
+                            <img
+                                src='/img/circle.png'
+                                className={`circleIcon ${item.id}`}
+                                onMouseEnter={iconMouseHoverEventHandler}
+                                onMouseLeave={iconMouseExitEventHandler}
+                                onClick={clickEventHandler}
+                                alt="이미지 깨짐"
+                            />
+                            <img
+                                src="/img/mouseClick.gif"
+                                className={`hideClick ${item.id} ${isIconHover === item.id ? 'removeClick' : ''}`}
+                                alt="이미지 깨짐"
+                            />
                             <p className="year">{item.year}</p>
                         </div>
                     ))}
@@ -80,6 +100,7 @@ const Main = () => {
                 </div>
 
             </section>
+
         </>
     );
 };
