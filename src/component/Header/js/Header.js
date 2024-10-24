d473018d88eb1e7b658465d18f1eddf06a75f57a
wonhwo <dlgy9714@gmail.com>
2024-10-24 18:00:46 +0900

    저장

--- src/component/Header/js/Header.js
+++ src/component/Header/js/Header.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import '../scss/Header.scss'
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <header id="headerContainer">
+            <div id="start">
+                <img src="/img/menu.png" alt="이미지 깨짐" className="menuBtn"/>
+                <div className="logoBox">
+                    <h1 className="logoTitle">
+                        <img src="/img/logo.png" className="logo" alt="로고없음"/>
+                        필효해</h1></div>
+            </div>
+            <div id="center">
+                <form action="" className="inputForm">
+
+                </form>
+            </div>
+            <div id="end">
+
+            </div>
+        </header>
     );
 };
 
