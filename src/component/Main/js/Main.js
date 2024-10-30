c1191500e00580c988fd03e3b8954eba2c66a3ba
wonhwo <dlgy9714@gmail.com>
2024-10-30 16:56:39 +0900

    메인 찐짜 아마 끝

--- src/component/Main/js/Main.js
+++ src/component/Main/js/Main.js
@@ -1,5 +1,5 @@
 import '../scss/Main.scss'
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {ReactTyped} from 'react-typed';
 import VideoPlatform from "../../VideoPlatform/js/VideoPlatform";
 
@@ -23,8 +23,8 @@ const Main = () => {
     const [isTitleCheck, setIsTitleCheck] = useState(false);
     const [isMoveTimeLine, setIsMoveTimeLine] = useState(false);
     const [isIconHover, seIsIconHover] = useState("");
+    const [isOneClickKnow, setIsOneClickKnow] = useState(0);
     const iconMouseHoverEventHandler = (e) => {
-        getPositonHandler(e);
         let temp = e.target.classList[1];
         seIsIconHover(temp);
     }
@@ -37,7 +37,7 @@ const Main = () => {
 
         // 클릭한 요소의 위치 계산
         const elementRect = clickedElement.getBoundingClientRect();
-        const $titleBox= document.querySelector(".titleBox");
+        const $titleBox = document.querySelector(".titleBox");
         $titleBox.style.left = `${elementRect.left + window.scrollX}px`;
         const elementCenterY = elementRect.top + window.scrollY + (elementRect.height / 2);
         const elementCenterX = elementRect.left + window.scrollX + (elementRect.width / 2);
@@ -59,24 +59,55 @@ const Main = () => {
     }
 
     const clickEventHandler = (e) => {
-        const findByYear = Array.from(e.target.parentNode.children).find(child => child.classList.contains('year'));
-        setWhatISYear(findByYear.textContent);
-        const dataElements = document.querySelectorAll('.timeLineBottom');
-        setIsMoveTimeLine(false);
-        dataElements.forEach((element) => {
-            element.classList.add('removeCircle');
-        });
-        setIsMoveTimeLine(true);
-
-        const titleHandler = () => {
-            setIsTitleCheck(true);
+        e.stopPropagation();
+        if(isOneClickKnow===0){
+                setIsOneClickKnow(1);
+                console.log("1클릭");
+            getPositonHandler(e);
 
         }
-        setTimeout(titleHandler, 1000);
+        if(isOneClickKnow===1){
+            if(isIconHover===e.target.classList[1]){
+                setIsOneClickKnow(2);
+                console.log("2클릭");
+                timeLineHandler(e);
+
+            }else{
+                getPositonHandler(e);
+                setIsOneClickKnow(0);
+            }
+        }
+        if(isOneClickKnow===2){
+            setIsOneClickKnow(0);
+            console.log("초기화")
+
+        }
+        iconMouseHoverEventHandler(e);
+
 
 
     }
+
+    const timeLineHandler = (e) => {
+            const findByYear = Array.from(e.target.parentNode.children).find(child => child.classList.contains('year'));
+            setWhatISYear(findByYear.textContent);
+            const dataElements = document.querySelectorAll('.timeLineBottom');
+            setIsMoveTimeLine(false);
+            dataElements.forEach((element) => {
+                element.classList.add('removeCircle');
+            });
+            setIsMoveTimeLine(true);
+
+            const titleHandler = () => {
+                setIsTitleCheck(true);
+            }
+            setTimeout(titleHandler, 1000);
+    }
     const downTimeLineHandler = () => {
+        if(isTitleCheck){
+        iconMouseExitEventHandler();
+        setIsOneClickKnow(0);
+
         const dataElements = document.querySelectorAll('.timeLineBottom');
         setIsTitleCheck(false);
 
@@ -84,13 +115,13 @@ const Main = () => {
         dataElements.forEach((element) => {
             element.classList.remove('removeCircle');
         });
-
+        }
     }
 
     return (
         <>
             <section id="timeLineContainer">
-                <div className={`timeLineBox ${isMoveTimeLine?"timeLineBoxTop":"timeLineBoxMid"}`} >
+                <div className={`timeLineBox ${isMoveTimeLine ? "timeLineBoxTop" : "timeLineBoxMid"}`}>
                     <div className="timeLine timeLineTop"
                          onClick={downTimeLineHandler}>
                         <div className="titleBox">
@@ -108,15 +139,8 @@ const Main = () => {
                             <div className={`data ${item.id}`}>
                                 <img
                                     src='/img/circle.png'
-                                    className={`circleIcon ${item.id}`}
-                                    onMouseEnter={iconMouseHoverEventHandler}
-                                    onMouseLeave={iconMouseExitEventHandler}
+                                    className={`circleIcon ${item.id} ${isIconHover === item.id ? 'circleIconSizeUp' : ''}`}
                                     onClick={clickEventHandler}
-                                    alt="이미지 깨짐"
-                                />
-                                <img
-                                    src="/img/mouseClick.gif"
-                                    className={`hideClick ${item.id} ${isIconHover === item.id ? 'removeClick' : ''}`}
                                     alt="이미지 깨짐"
                                 />
                                 <p className="year">{item.year}</p>
