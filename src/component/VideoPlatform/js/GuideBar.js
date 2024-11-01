ccf2ef95f7ff40ac15d5daba5dfac6d27829143d
wonhwo <dlgy9714@gmail.com>
2024-11-01 15:31:09 +0900

    저장

--- src/component/VideoPlatform/js/GuideBar.js
+++ src/component/VideoPlatform/js/GuideBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "../scss/GuideBar.scss"
+
 const GuideBar = () => {
     return (
         <div className="guideBar ">
@@ -11,19 +12,54 @@ const GuideBar = () => {
             </div>
             <div className="yearContainer GuidBarContainer">
                 <div className="year ContainerContent">
-                    <img className="yearIcon" src="" alt=""/>
+                    <p>2016년</p>
+                </div>                <div className="year ContainerContent">
+                    <p>2016년</p>
+                </div>                <div className="year ContainerContent">
+                    <p>2016년</p>
+                </div>                <div className="year ContainerContent">
+                    <p>2016년</p>
+                </div>                <div className="year ContainerContent">
+                    <p>2016년</p>
+                </div>                <div className="year ContainerContent">
+                    <p>2016년</p>
+                </div>                <div className="year ContainerContent">
+                    <p>2016년</p>
+                </div>                <div className="year ContainerContent">
+                    <p>2016년</p>
+                </div>                <div className="year ContainerContent">
                     <p>2016년</p>
                 </div>
             </div>
             <div className="monthContainer GuidBarContainer">
                 <div className="monthBar ContainerContent">
-                    <img className="monthIcon" src="" alt=""/>
+                    <p>8월</p>
+                </div>                <div className="monthBar ContainerContent">
+                    <p>8월</p>
+                </div>                <div className="monthBar ContainerContent">
+                    <p>8월</p>
+                </div>                <div className="monthBar ContainerContent">
+                    <p>8월</p>
+                </div>                <div className="monthBar ContainerContent">
+                    <p>8월</p>
+                </div>                <div className="monthBar ContainerContent">
+                    <p>8월</p>
+                </div>                <div className="monthBar ContainerContent">
+                    <p>8월</p>
+                </div>                <div className="monthBar ContainerContent">
+                    <p>8월</p>
+                </div>                <div className="monthBar ContainerContent">
+                    <p>8월</p>
+                </div>                <div className="monthBar ContainerContent">
+                    <p>8월</p>
+                </div>                <div className="monthBar ContainerContent">
+                    <p>8월</p>
+                </div>                <div className="monthBar ContainerContent">
                     <p>8월</p>
                 </div>
             </div>
             <div className="settingContainer GuidBarContainer">
                 <div className="setting ContainerContent">
-                    <img className="settingIcon" src="" alt=""/>
                     <p>설정</p>
                 </div>
             </div>
